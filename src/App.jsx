acd1e5f0890a7ec16dacb9344ab164e8977f2fe1
Kirill Komlev <kirillkomlev02@gmail.com>
2024-12-25 02:30:13 +0300

    загрузил реакт

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore quam, hic tempora commodi iste eos explicabo a itaque, mollitia nostrum voluptatem obcaecati blanditiis
+				accusantium, perferendis rem dignissimos suscipit cum laboriosam.
+			</p>
+		</>
+	)
+}
