c9080fa934f303adc00ec893fff9ae45dcbe9073
Kirill Komlev <kirillkomlev02@gmail.com>
2024-12-25 03:18:40 +0300

    задал базовые стили

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 export default function App() {
 	return (
 		<>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore quam, hic tempora commodi iste eos explicabo a itaque, mollitia nostrum voluptatem obcaecati blanditiis
-				accusantium, perferendis rem dignissimos suscipit cum laboriosam.
-			</p>
+			<header className='header'>
+				<div className='header__inner container'>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, nesciunt modi in quis vel debitis assumenda, laudantium alias, perspiciatis cum harum. Veritatis
+					molestias optio, dignissimos voluptas voluptatum in asperiores dolore.
+				</div>
+			</header>
 		</>
 	)
 }
